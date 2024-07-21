3da1084567e4fb15089a98b2a32ed7a897af4ce1
Do Trong Minh <80587574+trongminh13@users.noreply.github.com>
2024-07-21 21:07:22 +0700

    Du An Cua Thay Long

---
+++ src/test/java/com/example/Project207/Project207ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Project207;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project207ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
